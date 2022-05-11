2c1bfaa97d1a3e286c6092fd42b2e823e80ce333
shaunvan119 <shaunvan@live.com.au>
2022-05-11 15:24:23 +1000

    const styles in die.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,40 @@
 
 import React from "react"
 import Die from "./components/Die";
+import {nanoid} from "nanoid"
 
-export default  function App() {
-   const [dice, setDice] = React.useState(allNewDice())
+export default function App() {
 
-  function allNewDice() {
+
+    const [dice, setDice] = React.useState(allNewDice())
+    
+    function allNewDice() {
         const newDice = []
         for (let i = 0; i < 10; i++) {
-            newDice.push(Math.ceil(Math.random() * 6))
+            newDice.push({
+                value: Math.ceil(Math.random() * 6), 
+                isHeld: false,
+                id: nanoid()
+            })
         }
+        
         return newDice
     }
-
-    function rollDice() {
-      setDice(allNewDice())
-    }
-
-    const diceElements = dice.map(die => <Die value={die} />)
-   
-  return (
     
-      <main>
+    function rollDice() {
+        setDice(allNewDice())
+    }
+    
+    const diceElements = dice.map(die => <Die key={die.id} value={die.value} isHeld={die.isHeld} />)
+    
+    return (
+        <main>
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button className="roll-dice" onClick={rollDice}>Click</button>
+            <button className="roll-dice" onClick={rollDice}>Roll</button>
         </main>
-
-  );
+    )
 }
 
 
