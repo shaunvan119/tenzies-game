1d270a8f911ffca71fb3e8f3bfaa83091c9bcba2
shaunvan119 <shaunvan@live.com.au>
2022-05-11 14:52:07 +1000

    roll dice function

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ export default  function App() {
         return newDice
     }
 
+    function rollDice() {
+      setDice(allNewDice())
+    }
+
     const diceElements = dice.map(die => <Die value={die} />)
    
   return (
@@ -21,6 +25,7 @@ export default  function App() {
             <div className="dice-container">
                 {diceElements}
             </div>
+            <button onClick={rollDice}>Click</button>
         </main>
 
   );
