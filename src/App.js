3201684a80402fe517e273ded69314a5c3b0855f
shaunvan119 <shaunvan@live.com.au>
2022-05-11 14:54:47 +1000

    dice function

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default  function App() {
             <div className="dice-container">
                 {diceElements}
             </div>
-            <button onClick={rollDice}>Click</button>
+            <button className="roll-dice" onClick={rollDice}>Click</button>
         </main>
 
   );
